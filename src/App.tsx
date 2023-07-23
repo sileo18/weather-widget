c09888c7fc81d5587c28d4c0f19cadb83e89ce9a
Lucas Sileo <lucastoledo358@gmail.com>
2023-07-23 19:17:43 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,51 @@ import axios from "axios";
 import Widget from "./components/widget";
 import Input from "./components/input";
 import { useState } from "react";
+import classNames from "classnames";
+
+interface WeatherData {
+  main: {
+    temp: number;
+  };
+  // Outras propriedades da resposta da API, se houver
+}
 
 function App() {
+  const [city, setCity] = useState("");
 
-  const [city, setCity] = useState("")
+  const [shouldOpen, setShouldOpen] = useState(false);
 
-  const [shouldOpen , setShouldOpen] = useState(false)
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
 
-  const [weatherData, setWeatherData] = useState(null)
+  const tempInCelsius = weatherData?.main?.temp ?? 0;
 
-  const getWeather = () => {   
-
+  const getWeather = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=371a4f6b13f536664db98aae6dcd7441`;
 
-    axios.get(url).then((res) => {
-      const weatherData = res.data;
-      console.log(weatherData)
-      setShouldOpen(true)
-      setWeatherData(weatherData)    
-    }).catch((err) => {
-      console.log(err)
-    })
-
-    function getHours() {
-
-      const date = new Date()
-
-      const hour = date.getHours
-    } 
+    axios
+      .get<WeatherData>(url) // Anotação de tipo para a resposta da API
+      .then((res) => {
+        const weatherData = res.data;
+        setShouldOpen(true);
+        setWeatherData(weatherData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
-    <main className="bg-main min-h-screen flex justify-center items-center">
-      { shouldOpen ? <Widget city={city} weatherData={weatherData}  /> : <Input city={city} setCity={setCity} getWeather={getWeather}  /> }      
+    <main
+      className={classNames(
+        "bg-main min-h-screen flex justify-center items-center transition-all",
+        tempInCelsius < 22 ? "bg-custom" : "bg-custom1"
+      )}
+    >
+      {shouldOpen ? (
+        <Widget city={city} weatherData={weatherData} />
+      ) : (
+        <Input city={city} setCity={setCity} getWeather={getWeather} />
+      )}
     </main>
   );
 }
--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -3,29 +3,51 @@ import Switch from "./switch";
 
 interface WidgetProps {
   city: string;
-  weatherData: any
+  weatherData: any;
 }
 
 export default function Widget({ city, weatherData }: WidgetProps) {
+  const [typeTemp, setTypeTemp] = useState(false);
 
-  const [typeTemp, setTypeTemp] = useState(false)
+  const tempInCelsius = (weatherData.main.temp - 273.15).toFixed(2);
 
-  const tempInCelsius = (weatherData.main.temp - 273.15).toFixed(2)
+  const tempInFarenheit = (
+    ((weatherData.main.temp - 273.15) * 9) / 5 +
+    32
+  ).toFixed(2);
 
-  const tempInFarenheit = ((weatherData.main.temp - 273.15) * 9/5 + 32).toFixed(2)
+  const descriptionWeather = weatherData.weather[0].description
+
+  const firstCaracter = descriptionWeather.substring(0, 1).toUpperCase()
+
+  const rest = descriptionWeather.substring(1)
+
+  const descriptionWeatherFormated = firstCaracter + rest
+  
 
   return (
-    <div className="bg-white w-[600px] h-[600px] rounded-widget shadow-2xl flex flex-col items-center justify-start py-10">
+    <div className="bg-white w-[600px] h-[600px] rounded-widget shadow-2xl flex flex-col items-center justify-start py-10 gap-6">
       <Switch typeTemp={typeTemp} setTypeTemp={setTypeTemp} />
 
       <div className="relative">
-        <p className="text-deegres font-bold tracking-tight">{typeTemp ? tempInCelsius : tempInFarenheit}</p>
+        <p className="text-deegres font-bold tracking-tight">
+          {typeTemp ? tempInCelsius : tempInFarenheit}
+        </p>
         <span className="absolute top-[40px] right-[-32px] text-8xl">°</span>
       </div>
 
       <p className="text-4xl font-medium cursor-pointer">
-      {city.length > 25 ? city.slice(0, 25).concat("...") : city}
+        {city.length > 25
+          ? city.slice(0, 25).concat("...")
+          : city + ", " + weatherData.sys.country}
       </p>
+
+      <div className="flex gap-12">
+        <p className="flex flex-col justify-center items-center">          
+          <span className="text-lg">{descriptionWeatherFormated}</span>
+        </p>
+       
+      </div>
     </div>
   );
 }
